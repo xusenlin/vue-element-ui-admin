d564c6d7e58257b54e141a472748ce8c69bde8ba
737657495@qq.com <1468756621306qq>
2019-03-18 21:28:47 +0800

    remove the prefetch plugin
    去掉预取，

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    publicPath: './',
+    outputDir: undefined,
+    assetsDir: undefined,
+    runtimeCompiler: undefined,
+    productionSourceMap: undefined,
+    parallel: undefined,
+    chainWebpack: config => {
+        // remove the prefetch plugin
+        config.plugins.delete('prefetch')
+
+    }
+}
